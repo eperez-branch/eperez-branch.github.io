fa4cccfd706f780fe381a1746304047bebae998c
eperez-branch <125413972+eperez-branch@users.noreply.github.com>
2023-10-04 18:24:09 -0700

    Update script.js
    removed request body

--- myQRpage/script.js
+++ myQRpage/script.js
@@ -3,26 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
         const apiUrl = 'https://api2.branch.io/v2/qr-code';
 
         const requestBody = {
-            "qr_code_settings": {
-                "code_pattern": 3,
-                "finder_pattern": 1,
-                "image_format": "png",
-                "width": 500,
-                "code_color": "#000000",
-                "finder_pattern_color": "#000000",
-                "background_color": "#FFFFFF",
-                "center_logo_url": "https://cdn.branch.io/branch-assets//1153816943598261245/Number_1-1693347048370.png",
-                "margin": 1
-            },
-            "data": {
-                "$fallback_url": "http://musicapp.amazon.com/?ref=dmm_acq_dm_az_octnewmove_2023",
-                "$canonical_url": "https://musicapp.amazon.com",
-                "$marketing_title": "1 – Download the App AZ",
-                "~campaign": "dmm_acq_dm_az_octnewmove_2023",
-                "ref": "dmm_acq_dm_az_octnewmove_2023",
-                "~tags": ["1", "AZ"]
-            },
-            "branch_key": "key_live_ozpgeobWoV1PyOAvLLf5lomdwva66WYq"
         };
 
         const options = {
