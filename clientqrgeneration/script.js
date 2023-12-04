96f51be4e0cae570829550b375e602a09004d543
eperez-branch <125413972+eperez-branch@users.noreply.github.com>
2023-12-04 14:57:25 -0800

    Update script.js
    replaced values in 'data' with corresponding variables from user inputs

--- clientqrgeneration/script.js
+++ clientqrgeneration/script.js
@@ -1,28 +1,42 @@
 // listen for click on QR button
 document.getElementById('generateQRButton').addEventListener('click', function() {
+    // Gather inputs from form fields
+    var feature = document.getElementById('feature').value;
+    var channel = document.getElementById('channel').value;
+    var campaign = document.getElementById('campaign').value;
+    var tags = document.getElementById('tags').value.split(','); // Assuming tags are comma-separated
+    var ios_url = document.getElementById('ios_url').value;
+    var android_url = document.getElementById('android_url').value;
+    var desktop_url = document.getElementById('desktop_url').value;
+    var web_only = document.getElementById('web_only').checked; // Boolean
+    
     const options = {
         method: 'POST',
         headers: {accept: 'image/*', 'content-type': 'application/json'},
         body: JSON.stringify({
-          // QR code data
-          qr_code_settings: {
-            code_pattern: 2,
-            finder_pattern: 1,
-            image_format: 'png',
-            center_logo_url: 'https://cdn.branch.io/branch-assets//1153816943598261245/Listen_Now_2-1697049424813.png',
-            width: 500,
-            margin: 1,
-            code_color: '#0030FF',
-            background_color: '#FFFFFF',
-            finder_pattern_color: '#FF0000'
-          },
-          data: {
-            '$canonical_url': 'https://eperez-branch.github.io/',
-            '$fallback_url': 'https://eperez-branch.github.io/',
-            '~campaign': 'Website QR API Test',
-            '~channel': 'QR API'
-          },
-          branch_key: 'key_live_ozpgeobWoV1PyOAvLLf5lomdwva66WYq'
+            // QR code data
+            qr_code_settings: {
+                code_pattern: 2,
+                finder_pattern: 1,
+                image_format: 'png',
+                center_logo_url: 'https://cdn.branch.io/branch-assets//1153816943598261245/Listen_Now_2-1697049424813.png',
+                width: 500,
+                margin: 1,
+                code_color: '#0030FF',
+                background_color: '#FFFFFF',
+                finder_pattern_color: '#FF0000'
+            },
+            data: {
+                '~feature': feature,
+                '~channel': channel,
+                '~campaign': campaign,
+                '~tags':[tags],
+                '$ios_url': ios_url,
+                '$android_url': android_url,
+                '$desktop_url': desktop_url,
+                '$web_only': web_only
+            },
+            branch_key: 'key_live_ozpgeobWoV1PyOAvLLf5lomdwva66WYq'
         })
     };
     
