930fe797ee13763b88da3b68a7284c9fa25ca2e9
eperez-branch <125413972+eperez-branch@users.noreply.github.com>
2024-04-30 09:50:48 -0700

    Update script.js
    setBranchViewData in script

--- journeys_tests/script.js
+++ journeys_tests/script.js
@@ -1,1 +1,18 @@
+// Function to update the journey
+function updateJourney() {
+    // Close the current Branch journey
+    branch.closeJourney();
 
+    // Optional: Update link data if needed
+    var linkData = {
+        data: {
+            'ECID': 'MyTestECID',
+            'custom_int': Date.now(),
+            'custom_string': 'testing JS!'
+        }
+    };
+    branch.setBranchViewData(linkData);
+
+    // Track a pageview event, which could reload the banner
+    branch.track('pageview');
+}
